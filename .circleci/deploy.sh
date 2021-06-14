eb1e5fcffc547ac339566bd24a36fd39356e45ea
mlc <winhtet1997@gmail.com>
2021-06-14 00:50:06 -0700

    change branch

---
+++ .circleci/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "* checking out the master branch:"
+git clone --single-branch --branch master git@github.com:winhtet-mlc/circlecitest.git master
+
+echo "* synchronizing the files:"
+rsync -arv public/ master --delete --exclude ".git"
+cp README.md master/
+
+echo "* pushing to master:"
+cd master
+git config user.name "CircleCI"
+git config user.email ${GIT_EMAIL}
+git add -A
+git commit -m "Automated deployment job ${CIRCLE_BRANCH} #${CIRCLE_BUILD_NUM} [skip ci]" --allow-empty
+git push origin master
+
+echo "* done"
